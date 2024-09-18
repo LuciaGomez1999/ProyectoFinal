a041185fd024635dca529cdfcc7558b35bec350f
Lucia Gomez <Luciagomeez@hotmail.com>
2024-09-18 20:11:30 -0300

    Agregadas clase reserva y habitacion.

---
+++ proyectoSantini/src/gestionHabitaciones.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class gestionHabitaciones {
+    private JPanel panelHabitaciones;
+    private JButton modificarButton;
+    private JButton eliminarButton;
+    private JTable tablaGestionHabit;
+    private JButton agregarButton;
+    private JTextField textField2;
+    private JComboBox comboBoxTipo;
+    private JComboBox comboBoxCantidad;
+
+    public gestionHabitaciones() {
+        agregarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
